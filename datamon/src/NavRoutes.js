a8879775d861baf876a627b263daa85b89d7399e
Ali Bakhtiari <alibakh62@gmail.com>
2020-08-24 22:16:38 -0700

    added some button styling

--- datamon/src/NavRoutes.js
+++ datamon/src/NavRoutes.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
+
+const getStartedTheme = createMuiTheme({
+  typography: {
+    fontFamily: "Nunito",
+    fontWeight: 400,
+    fontSize: 12
+  },
+  spacing: {
+    marginLeft: 1
+  }
+});
+
+const loginTheme = createMuiTheme({
+  // palette: {
+  //   primary: HUE[100]
+  // },
+  typography: {
+    fontFamily: "Nunito",
+    fontWeight: 400,
+    fontSize: 12
+  },
+  spacing: {
+    marginRight: 1,
+    marginTop: 3
+  }
+});
 
 const useStyles = makeStyles((theme) => ({
   nav_auth: {
     '& > *': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(3),
       borderRadius: 25,
-      fontFamiliy: 'Nunito',
+      fontFamiliy: 'Nunito'
     },
   },
 }));
@@ -20,13 +47,22 @@ const GetStarted = () => {
   }
 
   return (
-    <div className={classes.nav_auth}>
-      <Button variant="contained" 
-              color="secondary" 
-              onClick={handleClick}>
-        Get Started
-      </Button>
-    </div>
+      <div className={classes.nav_auth}>
+        <ThemeProvider theme={loginTheme}>
+          <Button variant="contained"
+                  color="primary" 
+                  onClick={handleClick}>
+            Log in
+          </Button>
+        <ThemeProvider theme={getStartedTheme}>
+          <Button variant="contained" 
+                  color="secondary" 
+                  onClick={handleClick}>
+            Get Started
+          </Button>
+        </ThemeProvider>
+        </ThemeProvider>
+      </div>
   );
 }
 
