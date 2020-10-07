84bdf29a0ccbb623281deccb6359f85e77146ed8
Ali Bakhtiari <alibakh62@gmail.com>
2020-10-07 13:00:55 -0500

    added context API

--- Datamon-phone/App.js
+++ Datamon-phone/App.js
@@ -1,27 +1,31 @@
-import 'react-native-gesture-handler';
+import 'react-native-gesture-handler'
 import * as React from 'react';
 import Projects from './screens/Projects'
 import Landing from './screens/Landing'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { StateProvider } from './StateProvider'
+import reducer, { initialState } from './reducer'
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Projects}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen 
-          name="Landing" 
-          component={Landing} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Projects}
+            options={{ title: 'Welcome' }}
+          />
+          <Stack.Screen 
+            name="Landing" 
+            component={Landing} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StateProvider>
   );
 };
 
